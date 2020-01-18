766b285df896defac40fb2382b73c35791e225bd
payney1701 <robbiepayne1701@gmail.com>
2020-01-18 21:54:56 +0000

    figure4 added

--- figure1.py
+++
@@ -1,5 +0,0 @@
-WIDTH = 700
-HEIGHT = 800
-
-def draw():
-    screen.fill((128, 128, 128))
--- figure2.py
+++
@@ -1,5 +0,0 @@
-def makeTrack(): # Function to make a new section of track
-    global trackCount, trackLeft, trackRight, trackPosition, trackWidth
-    trackLeft.append(actor("barrier", pos = (trackPosition-trackWidth,0)))
-    trackRight.append(actor("barrier", pos = (trackPosition+trackWidth,0)))
-    trackCount += 1
